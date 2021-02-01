4b683fbc05a743a0b8ad4394a44e9c2311b54d39
Mauricio Badilla <mbadilla86@gmail.com>
2021-02-01 15:17:01 -0600

    module cycles

---
+++ 4_solo_pares2.rb
@@ -0,0 +1,4 @@
+n = ARGV[0].to_i 
+(2 * n).times do |n|
+    print " #{n + 1} " if (n + 1).even?
+end
---
+++ 7_lorem_generator.rb
@@ -0,0 +1,4 @@
+numero_de_parrafos_imprimir= ARGV[0].to_i #asignando la variable N
+parrafo= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus neque erat, finibus sed diam eget, elementum convallis leo. Quisque sagittis vitae sem a consequat. Morbi eget justo non tortor tristique dapibus quis ut velit. Quisque quis placerat purus, at facilisis dui. Nam faucibus eu erat non efficitur. Duis non neque a diam tincidunt mattis vel nec sapien. Nullam scelerisque diam ut aliquet varius. In nunc nisl, ultricies quis tincidunt eu, tempus vitae eros. Donec tempus laoreet urna, vitae pharetra mi vulputate rutrum." #
+
+numero_de_parrafos_imprimir.times { puts "\n#{parrafo} \n"}
